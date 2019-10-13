638e4a7232713e84d72bf8810876ce3798ae3142
Jewel Rana <jewel@ticonsys.com>
2019-10-13 15:55:30 +0600

    complete remote datasource test

--- test/features/number_trivia/data/sources/number_trivia_local_data_source_test.dart
+++ test/features/number_trivia/data/sources/number_trivia_local_data_source_test.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 
+import 'package:clean_architecture_tdd_course/core/error/exceptions.dart';
 import 'package:clean_architecture_tdd_course/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:clean_architecture_tdd_course/features/number_trivia/data/sources/number_trivia_local_data_source.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:matcher/matcher.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../../fixtures/fixture_reader.dart';
@@ -35,6 +37,32 @@ void main(){
       //assert
       verify(mockSharedPreferences.getString(CACHED_NUMBER_TRIVIA));
       expect(result, equals(tNumberTriviaModel));
+    });
+
+    test('Should throw CacheException when there is not cached value.',
+    () async {
+      //arrange
+      when(mockSharedPreferences.getString(any))
+      .thenReturn(null);
+      //act
+      final call = dataSource.getLastNumberTrivia;
+      //assert
+      expect(() => call(), throwsA(TypeMatcher<CacheException>()));
+    });
+
+  });
+
+  group('cacheNumberTrivia', (){
+    final tNumberTriviaModel = NumberTriviaModel(number: 1, text: "test trivia");
+    test(
+    'should call SharedPreferences to cache the data',
+    () async{
+      //act
+      dataSource.cacheNumberTrivia(tNumberTriviaModel);
+      //assert
+      final expectedJsonString = json.encode(tNumberTriviaModel.toJson());
+      verify(mockSharedPreferences.setString(CACHED_NUMBER_TRIVIA, expectedJsonString));
+      
     });
   });
 }
