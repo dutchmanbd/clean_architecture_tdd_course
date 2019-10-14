6bb71043cb92c59d0f73723469c3ce6064270988
Jewel Rana <jewel@ticonsys.com>
2019-10-14 18:48:31 +0600

    partial complete last tutorial

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,10 +1,29 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
+import 'package:clean_architecture_tdd_course/core/util/input_converter.dart';
+import 'package:clean_architecture_tdd_course/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:clean_architecture_tdd_course/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import './bloc.dart';
+import 'package:meta/meta.dart';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc({
+    @required GetConcreteNumberTrivia concrete, 
+    @required GetRandomNumberTrivia random, 
+    @required this.inputConverter
+  }): 
+  assert(concrete != null),
+  assert(random != null),
+  assert(inputConverter != null),
+  getConcreteNumberTrivia = concrete,
+  getRandomNumberTrivia = random;
+  
   @override
-  NumberTriviaState get initialState => InitialNumberTriviaState();
+  NumberTriviaState get initialState => Empty();
 
   @override
   Stream<NumberTriviaState> mapEventToState(
